921869cb4c2e8404838cb85294bacc23b856abf7
Deokgon Kim <dgkim@dgkim.net>
2026-06-28 21:15:14 +0000

    add env.example.sh

---
+++ shell-scripts/env.example.sh
@@ -0,0 +1,7 @@
+# Used by setup/setup-postfix-aws-smtp.sh
+AWS_SMTP_HOST="email-smtp.ap-northeast-2.amazonaws.com"
+AWS_SMTP_PORT="587"
+AWS_SMTP_USERNAME="redacted"
+AWS_SMTP_PASSWORD="redacted"
+AWS_SMTP_FROM="root@example.com"
+
