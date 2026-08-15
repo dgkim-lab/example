118d693d720e6047c5c5d4fe84b6cc944e8e226c
Deokgon Kim <dgkim@dgkim.net>
2026-08-15 16:43:53 +0900

    update heic2jpg.sh to support single file processing

--- ffmpeg/heic2jpg.sh
+++ ffmpeg/heic2jpg.sh
@@ -3,7 +3,11 @@
 #done
 
 # 대소문자 구분 없이 *.HEIC, *.heic 모두 처리
-for f in *.[hH][eE][iI][cC]; do
-    convert "$f" -auto-orient "${f%.*}.jpg"
-done
+if [ -z "$1" ]; then
+    for f in *.[hH][eE][iI][cC]; do
+        convert "$f" -auto-orient "${f%.*}.jpg"
+    done
+else
+    convert "$1" -auto-orient "${1%.*}.jpg"
+fi
 echo "회전 적용 및 JPG 변환 완료!"
